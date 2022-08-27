3cb67c32db68a9f9c3408e75f6ff1849735a8c2a
aman007shrestha <aman13xrestha@gmail.com>
2022-08-27 23:19:25 +0545

    Add DB migrations

---
+++ src/constants/constants.ts
@@ -0,0 +1,18 @@
+// database
+export const USER_ACCOUNT_TABLE = "user_account";
+
+export const USER_ACCOUNT_SCHEMA = {
+  ID: "user_id",
+  EMAIL: "email",
+  PASSWORD: "password",
+};
+
+export const SET_NULL = "SET NULL";
+// Controllers
+export const INPUT_ALL_FIELDS_MESSAGE = "Input All Fields";
+
+// Index
+export const TRANSPORT_LIMIT = "50mb";
+
+// UTILS
+export const JWT_VALIDITY = "30d";
---
+++ src/db/migrations/20220827170324_create_user_account_table.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+import {
+  USER_ACCOUNT_TABLE,
+  USER_ACCOUNT_SCHEMA,
+} from "../../constants/constants";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(USER_ACCOUNT_TABLE, (table) => {
+    table.increments(USER_ACCOUNT_SCHEMA.ID);
+    table.string(USER_ACCOUNT_SCHEMA.EMAIL).notNullable().unique();
+    table.string(USER_ACCOUNT_SCHEMA.PASSWORD).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(USER_ACCOUNT_TABLE);
+}
